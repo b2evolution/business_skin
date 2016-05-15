9cec6861fd1cb6e89529b2a52ae6ebbcc23f60cf
Ariflaw <nurariflaw@gmail.com>
2016-05-16 06:20:15 +0700

    Update styling Flowing Blocks

--- _front.disp.php
+++ _front.disp.php
@@ -29,21 +29,22 @@ $params = array_merge( array(
 <div class="evo_container evo_container__front_page_primary">
    <?php
    skin_container( NT_('Front Page Main Area'), array(
-   	// The following params will be used as defaults for widgets included in this container:
-   	'author_link_text'        => $params['author_link_text'],
-   	'featured_intro_before'   => $params['featured_intro_before'],
-   	'featured_intro_after'    => $params['featured_intro_after'],
-   	'block_start'             => $params['front_block_start'],
-   	'block_end'               => $params['front_block_end'],
-   	'block_first_title_start' => $params['front_block_first_title_start'],
-   	'block_first_title_end'   => $params['front_block_first_title_end'],
-   	'block_title_start'       => $params['front_block_title_start'],
-   	'block_title_end'         => $params['front_block_title_end'],
-      // Search
-      'search_input_before'     => '<div class="input-group">',
-      'search_input_after'      => '',
-      'search_submit_before'    => '<span class="input-group-btn">',
-      'search_submit_after'     => '</span></div>',
+	   // The following params will be used as defaults for widgets included in this container:
+	   'author_link_text'        => $params['author_link_text'],
+	   'featured_intro_before'   => $params['featured_intro_before'],
+	   'featured_intro_after'    => $params['featured_intro_after'],
+	   'block_start'             => $params['front_block_start'],
+	   'block_end'               => $params['front_block_end'],
+	   'block_first_title_start' => $params['front_block_first_title_start'],
+	   'block_first_title_end'   => $params['front_block_first_title_end'],
+	   'block_title_start'       => $params['front_block_title_start'],
+	   'block_title_end'         => $params['front_block_title_end'],
+
+	   // Search
+	   'search_input_before'     => '<div class="input-group">',
+	   'search_input_after'      => '',
+	   'search_submit_before'    => '<span class="input-group-btn">',
+	   'search_submit_after'     => '</span></div>',
    ) );
    // --------------------- END OF "Front Page Main Area" CONTAINER -----------------------
    ?>
