7bd28106f99ff49bf0f8aa56a916a8df881c7080
Milos Zivojinovic <miloszivojinovic93@gmail.com>
2016-06-15 12:08:08 +0200

    timeline fix for regular layout on disp=posts

--- _skin.class.php
+++ _skin.class.php
@@ -17,7 +17,7 @@ if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.'
  */
 class business_Skin extends Skin
 {
-	var $version = '1.2.1';
+	var $version = '1.3.0';
 	/**
 	 * Do we want to use style.min.css instead of style.css ?
 	 */
