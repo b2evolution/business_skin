8d003950d299d6aeb194baac9386a279ba9ea265
Milos Zivojinovic <miloszivojinovic93@gmail.com>
2016-08-11 18:47:24 +0200

    LESS modules include + layout fix for floated post content images

--- _item_content.inc.php
+++ _item_content.inc.php
@@ -51,7 +51,7 @@ $params = array_merge( array(
 	'excerpt_more_text'        => T_( 'Read More' ). $icon_mexpt_mb,
 
 	// In case we display a full version of the post:
-	'content_start_full_text'  => '<div class="evo_post__full_text">',
+	'content_start_full_text'  => '<div class="evo_post__full_text clearfix">',
 	'content_end_full_text'    => '</div>',
 
 	'before_content_teaser'    => '',
