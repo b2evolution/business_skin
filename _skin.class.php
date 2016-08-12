6bc956fd5e3b3f9a4c372ebfffbfc3a8f0f99f89
Milos Zivojinovic <miloszivojinovic93@gmail.com>
2016-08-12 13:35:48 +0200

    update skin version

--- _skin.class.php
+++ _skin.class.php
@@ -17,7 +17,7 @@ if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.'
  */
 class business_Skin extends Skin
 {
-	var $version = '1.3.2';
+	var $version = '1.3.1';
 	/**
 	 * Do we want to use style.min.css instead of style.css ?
 	 */
