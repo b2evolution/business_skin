b453eb87824d4896b620c399733c92389ce791b4
yurabakhtin <yurybakh@gmail.com>
2018-07-06 11:36:04 +0300

    Update widget containers code for proper editing on customizer designer mode

--- _item_block.inc.php
+++ _item_block.inc.php
@@ -135,9 +135,10 @@ echo '<div class="evo_content_block '.$post_item.$column.'">'; // Beginning of p
 		{
 			// ------------------------- "Item Single - Header" CONTAINER EMBEDDED HERE --------------------------
 			// Display container contents:
-			skin_container( /* TRANS: Widget container name */ NT_('Item Single Header'), array(
+			widget_container( 'item_single_header', array(
 				'widget_context' => 'item',	// Signal that we are displaying within an Item
 				// The following (optional) params will be used as defaults for widgets included in this container:
+				'container_display_if_empty' => false, // If no widget, don't display container at all
 				// This will enclose each widget in a block:
 				'block_start' => '<div class="evo_widget $wi_class$">',
 				'block_end' => '</div>',
@@ -160,12 +161,11 @@ echo '<div class="evo_content_block '.$post_item.$column.'">'; // Beginning of p
 	if( $disp == 'single' )
 	{
 		// ------------------------- "Item Single" CONTAINER EMBEDDED HERE --------------------------
+		// Display container contents:
 		widget_container( 'item_single', array(
 			'widget_context' => 'item',	// Signal that we are displaying within an Item
 			// The following (optional) params will be used as defaults for widgets included in this container:
 			'container_display_if_empty' => false, // If no widget, don't display container at all
-			'container_start' => '<div class="evo_container $wico_class$">',
-			'container_end'   => '</div>',
 			// This will enclose each widget in a block:
 			'block_start' => '<div class="evo_widget $wi_class$">',
 			'block_end' => '</div>',
@@ -196,14 +196,12 @@ echo '<div class="evo_content_block '.$post_item.$column.'">'; // Beginning of p
 	}
 	elseif( $disp == 'page' )
 	{
-		?>
-		<div class="evo_container evo_container__item_page">
-		<?php
 		// ------------------------- "Item Page" CONTAINER EMBEDDED HERE --------------------------
 		// Display container contents:
-		skin_container( /* TRANS: Widget container name */ NT_('Item Page'), array(
+		widget_container( 'item_page', array(
 			'widget_context' => 'item',	// Signal that we are displaying within an Item
 			// The following (optional) params will be used as defaults for widgets included in this container:
+			'container_display_if_empty' => false, // If no widget, don't display container at all
 			// This will enclose each widget in a block:
 			'block_start' => '<div class="evo_widget $wi_class$">',
 			'block_end' => '</div>',
@@ -231,9 +229,6 @@ echo '<div class="evo_content_block '.$post_item.$column.'">'; // Beginning of p
 				),
 		) );
 		// ----------------------------- END OF "Item Page" CONTAINER -----------------------------
-		?>
-		</div>
-		<?php
 	}
 	else
 	{
